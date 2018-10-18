2cd9eca1c3895cc5f48edc6635517dd988ee1f87
Lotus <cmadisonne@gmail.com>
2018-10-17 23:31:08 -0500

    Fixed Root URL

--- blogProject/blogApp/urls.py
+++ blogProject/blogApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('user/', views.userIndex, name='user_index'),
+    path('index/', views.index, name='index'),
+    path('', views.userIndex, name='user_index'),
     path('news/feed/',views.newsFeed, name='newsFeed')
 ]
